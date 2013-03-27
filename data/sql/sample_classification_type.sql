4b82843903fcc47be59f8d596c951f9e3fd80d72
Ian Wright <ian.wright@well.ox.ac.uk>
2013-03-27 10:07:13 +0000

    Change charset to utf8

--- data/sql/sample_classification_type.sql
+++ data/sql/sample_classification_type.sql
@@ -5,7 +5,7 @@ CREATE TABLE `sample_classification_type` (
   `description` text,
   `ordr` int(11) DEFAULT NULL,
   PRIMARY KEY (`sample_classification_type`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 INSERT INTO `sample_classification_type` VALUES ('region','Region','Used for the calculation of genetic marker frequencies on a detailed geographical scale.',2),('subcont','Population','Used for the calculation of SNP allele frequencies.',1);
 
