585a438f8460aaf8eccbbf938fd5d0dd0ddf7f41
Ian Wright <ian.wright@well.ox.ac.uk>
2013-04-04 12:50:11 +0100

    Hackery to load SampleGroups table

--- data/sql/merge_sample_contexts.sql
+++ data/sql/merge_sample_contexts.sql
@@ -1,1 +1,7 @@
 UPDATE sample_contexts sc LEFT JOIN sample_context_description scd ON sc.sample_context = scd.sample_context SET sc.description = scd.description;
+
+INSERT INTO sample_classifications (sample_classification, sample_classification_type_id) SELECT DISTINCT `Group`, 'region' FROM SampleGroups WHERE `Group` NOT IN (SELECT sample_classification FROM sample_classifications);
+
+truncate table sample_classifications_samples;
+insert into sample_classifications_samples (sampleclassification_id, sample_id) SELECT `Group`, Sample FROM SampleGroups;
+
