12d865e681652d1c898fa21e1a22e64a6a68ab8f
Ian Wright <ian.wright@well.ox.ac.uk>
2013-04-15 11:33:53 +0100

    Don't exclude lines with context

--- data/sql/check_data.sh
+++ data/sql/check_data.sh
@@ -1,6 +1,6 @@
 for j in *
 do
-  for i in `grep -A 1 '(' ${j} | grep -v ')' | grep -v '(' | grep , | grep -v double | grep -v text | grep -v int | sed -e 's/ //g'`
+  for i in `grep -A 1 '(' ${j} | grep -v ')' | grep -v '(' | grep , | grep -v double | grep -v ' text' | grep -v int | sed -e 's/ //g'`
   do
 	FILE=${j}
 	HEAD=`echo $i | sed -e "s/${FILE}-//" -e 's/\`//g'`
