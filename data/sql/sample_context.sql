5380f05c927951ae0916c0064f017c316c9c7077
Ian Wright <ian.wright@well.ox.ac.uk>
2013-04-19 13:55:37 +0100

    Fix col name

--- data/sql/sample_context.sql
+++ data/sql/sample_context.sql
@@ -1,1 +1,1 @@
-CREATE OR REPLACE VIEW sample_contexts AS select sample_context_id, title, description, Study as study_id, location_id from sample_with_context where Exclude = 'FALSE' group by sample_context;
+CREATE OR REPLACE VIEW sample_contexts AS select sample_context_id, title, description, Study as study_id, location_id from sample_with_context where Exclude = 'FALSE' group by sample_context_id;
