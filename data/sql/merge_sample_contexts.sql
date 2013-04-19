7ed4e221f162e086adab0640b2652d81c44dc6f7
Ian Wright <ian.wright@well.ox.ac.uk>
2013-04-19 15:06:57 +0100

    , not tab separated

--- data/sql/merge_sample_contexts.sql
+++ data/sql/merge_sample_contexts.sql
@@ -1,19 +1,27 @@
 UPDATE sample_contexts sc LEFT JOIN sample_context_description scd ON sc.sample_context = scd.sample_context SET sc.description = scd.description;
 
-INSERT INTO sample_classifications (ordr, sample_classification, sample_classification_type_id, name, lattit, longit) 
+INSERT INTO sample_classifications (ordr, sample_classification, sample_classification_type, name, lattit, longit) 
 SELECT ordr,sample_classification,sample_classification_type,name,longit,lattit FROM sample_classifications1;
 
-INSERT INTO sample_classifications (sample_classification, sample_classification_type_id, name, lattit, longit) 
+INSERT INTO sample_classifications (sample_classification, sample_classification_type, name, lattit, longit) 
 SELECT DISTINCT `Pop`, 'subcont', name, lattit, longit FROM SampleGroups sg LEFT JOIN `Definition-Populations` dp ON dp.ID = sg.`Pop` WHERE `Pop` NOT IN (SELECT sample_classification FROM sample_classifications);
 
-INSERT INTO sample_classifications (sample_classification, sample_classification_type_id, name, lattit, longit) 
+INSERT INTO sample_classifications (sample_classification, sample_classification_type, name, lattit, longit) 
 SELECT DISTINCT Country, 'region', name, lattit, longit FROM `metadata-2.2_withsites` md LEFT JOIN `Definition-Regions` dr ON dr.ID = md.Country WHERE Exclude = 'FALSE' AND `Country` NOT IN (SELECT sample_classification FROM sample_classifications);
 
-delete sample_classifications_samples from sample_classifications_samples LEFT JOIN sample_classifications ON sample_classifications_samples.sampleclassification_id = sample_classifications.sample_classification where sample_classification_type_id = 'subcont';
+DROP TABLE IF EXISTS `sample_classifications_samples`;
+CREATE TABLE `sample_classifications_samples` (
+  `sample_id` varchar(50) DEFAULT NULL,
+  `sampleclassification_id` varchar(50) DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+delete sample_classifications_samples from sample_classifications_samples LEFT JOIN sample_classifications ON sample_classifications_samples.sampleclassification_id = sample_classifications.sample_classification where sample_classification_type = 'subcont';
 
 insert into sample_classifications_samples (sampleclassification_id, sample_id) SELECT `Pop`, Sample FROM SampleGroups;
 
-delete sample_classifications_samples from sample_classifications_samples LEFT JOIN sample_classifications ON sample_classifications_samples.sampleclassification_id = sample_classifications.sample_classification where sample_classification_type_id = 'region';
+delete sample_classifications_samples from sample_classifications_samples LEFT JOIN sample_classifications ON sample_classifications_samples.sampleclassification_id = sample_classifications.sample_classification where sample_classification_type = 'region';
 
 insert into sample_classifications_samples (sampleclassification_id, sample_id) select Country,Sample from `metadata-2.2_withsites` md LEFT JOIN sample_classifications_samples scs ON scs.sample_id = md.Sample  where Exclude='FALSE';
 
