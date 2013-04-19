d71cc2a1f94a41772e35cba118d546c7ebfef5e2
Ian Wright <ian.wright@well.ox.ac.uk>
2013-04-19 14:00:26 +0100

    Fix col name

--- data/sql/sample_context.sql
+++ data/sql/sample_context.sql
@@ -1,3 +1,4 @@
+DROP VIEW sample_context;
 CREATE OR REPLACE VIEW sample_context AS select sample_context_id, title, description, Study as study_id, location_id from sample_with_context where Exclude = 'FALSE' group by sample_context_id;
 DROP TABLE IF EXISTS `sample_contexts`;
-CREATE TABLE sample_contexts AS SELECT * FROM sample_context;
+CREATE TABLE sample_contexts AS SELECT sample_context_id as sample_context, title, description, Study as study_id, location_id FROM sample_context;
